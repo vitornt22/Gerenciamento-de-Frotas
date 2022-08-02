c28b52ed34da800581e9c62780d6ed4f78d5879d
Vitor Neto <vitornt434@gmail.com>
2022-08-02 17:33:02 -0300

    changes

--- Optar/settings.py
+++ Optar/settings.py
@@ -177,10 +177,3 @@ DATE_INPUT_FORMATS = ('%d/%m/%Y',)
 
 
 # Email Settings
-DEFAULT_FROM_EMAIL = "vitornt434@gmail.com"
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST = config('EMAIL_HOST')
